abfb676b3257e72579f30ac9e873b73fdf2d3418
derek-lu <delu@adobe.com>
2015-01-26 10:48:13 -0800

    Added statement to delete from groups_for_users

--- php/deleteGroup.php
+++ php/deleteGroup.php
@@ -2,10 +2,12 @@
 require_once "settings.php";
 require_once "utils.php";
 
+// ini_set('display_errors', 1);
+
 $guid = escapeURLData($_POST["guid"]);
 $csrfToken = escapeURLData($_POST["csrfToken"]);
 
-$mysqli = new mysqli($db_host, $db_user, $db_password, "entitlement_admin");
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 if (!isValidCsrfToken($mysqli, $guid, $csrfToken)) {
 	echo '{"success":false,"description":"Sorry, invalid token."}'; 
@@ -23,16 +25,17 @@ if (!isValidCsrfToken($mysqli, $guid, $csrfToken)) {
 					$stmt->execute();
 					$stmt->close();
 					
-					if ($stmt = $mysqli->prepare("DELETE FROM folios_for_groups WHERE group_id = ? AND guid = ?")) {
-						if ($stmt->bind_param("ss", $id, $guid)) {
-							$stmt->execute();
-							echo '{"success":true}';
-						} else {
-							echo '{"success":false,"description":"deleteGroup - Binding folios_for_groups parameters failed: (' . $mysqli->errno . ')' . $mysqli->error . '"}';
-						}
-					} else {
-						echo '{"success":false,"description":"deleteGroup - Prepare folios_for_groups failed: (' . $mysqli->errno . ')' . $mysqli->error . '"}';
-					}
+					// Delete from folios_for_groups
+					$stmt = $mysqli->prepare("DELETE FROM folios_for_groups WHERE group_id = ? AND guid = ?");
+					$stmt->bind_param("ss", $id, $guid);
+					$stmt->execute();
+
+					// Delete from groups_for_users
+					$stmt = $mysqli->prepare("DELETE FROM groups_for_users WHERE group_id = ? AND guid = ?");
+					$stmt->bind_param("ss", $id, $guid);
+					$stmt->execute();
+					
+					echo '{"success":true}';
 				} else {
 					echo '{"success":false,"description":"deleteGroup - Binding groups parameters failed: (' . $mysqli->errno . ')' . $mysqli->error . '"}';
 				}
